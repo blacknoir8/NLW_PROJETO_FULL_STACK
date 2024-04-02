3dbaf17df22773ee24586866183e3f7f2b57a812
Iago Gomes Oliveira <111358641+blacknoir8@users.noreply.github.com>
2024-04-02 16:55:08 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,19 @@
 let participantes = [
     {
-      nome: "Diego Fernandes",
-      email: "diego@gmail.com",
+      nome: "Iago Gomes",
+      email: "iago@gmail.com",
       dataInscricao: new Date(2024, 2, 1, 19, 23),
       dataCheckIn: new Date(2024, 2, 1, 20, 20)
     },
     {
-      nome: "Mayk Brito",
-      email: "mayk@gmail.com",
+      nome: "Sabrina Leita",
+      email: "sabrina@gmail.com",
       dataInscricao: new Date(2024, 2, 23, 19, 23),
       dataCheckIn: null
     },
     {
-      nome: "Ana Souza",
-      email: "ana@gmail.com",
+      nome: "Marcela Oliveira",
+      email: "marcela@gmail.com",
       dataInscricao: new Date(2024, 0, 3, 19, 23),
       dataCheckIn: new Date(2024, 0, 4, 20, 20)
     },
@@ -24,8 +24,8 @@ let participantes = [
       dataCheckIn: new Date(2023, 11, 5, 20, 20)
     },
     {
-      nome: "Maria Oliveira",
-      email: "maria@gmail.com",
+      nome: "Brunno Oliveira",
+      email: "brunno@gmail.com",
       dataInscricao: new Date(2023, 10, 5, 19, 23),
       dataCheckIn: null
     },
@@ -36,14 +36,14 @@ let participantes = [
       dataCheckIn: new Date(2023, 9, 7, 20, 20)
     },
     {
-      nome: "Carla Lima",
-      email: "carla@gmail.com",
+      nome: "Gabriela Gomes",
+      email: "gabriela@gmail.com",
       dataInscricao: new Date(2023, 8, 7, 19, 23),
       dataCheckIn: new Date(2023, 8, 8, 20, 20)
     },
     {
-      nome: "Lucas Sousa",
-      email: "lucas@gmail.com",
+      nome: "Thaine Santana",
+      email: "thaine@gmail.com",
       dataInscricao: new Date(2023, 7, 8, 19, 23),
       dataCheckIn: new Date(2023, 7, 9, 20, 20)
     },
