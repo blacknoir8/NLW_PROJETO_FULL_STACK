2556abb3deb252d1d94704d57b35a0d08a676e99
Iago Gomes Oliveira <111358641+blacknoir8@users.noreply.github.com>
2024-04-02 14:06:34 -0300

    Update script.js

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const criarNovoParticipante =()=>{
+  return'alo'
+}
+
+
+const atualuzarLista = () => {
+// pegar informação do HMTL 
+
+
+
+
+//SUBSTITUIR INFORMAÇÃO DO HMTL 
+document
+.querySelector('tbody')
+.innerHTML='alo'
+}
+
+atualuzarLista()
